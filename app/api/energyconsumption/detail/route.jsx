69f57a4d15016b63bb43b06dd47186b1bf198660
ManishMaster <manish.rsauto@gmail.com>
2023-11-20 16:18:52 +0530

    use static date

--- app/api/energyconsumption/detail/route.jsx
+++ app/api/energyconsumption/detail/route.jsx
@@ -3,11 +3,13 @@ import { query } from "@/app/lib/database";
 
 export async function POST(req) {
   const data = await req.json()
-   const em = data.data.energymeter;
-   const date= data.data.date.split("-")
-   const shift = data.data.shift
-    try {
-        const sql = `SELECT * FROM rishabhplastic.rishabhem${em>9?em:`0${em}`} WHERE DATE = '${date[2]}-${date[1]}-${date[0]}' AND shift = ${shift}`;
+  const em = data.data.energymeter;
+  const date= data.data.date.split("-")
+  const shift = data.data.shift
+  
+  try {
+      const sql = `SELECT * FROM rishabhplastic.rishabhem${em>9?em:`0${em}`} WHERE DATE = '18-06-2023' AND shift = ${shift}`;
+      // const sql = `SELECT * FROM rishabhplastic.rishabhem${em>9?em:`0${em}`} WHERE DATE = '${date[2]}-${date[1]}-${date[0]}' AND shift = ${shift}`;
         const users = await query({
           query: sql,
           values: [],
