c7fa614421ae0e75a62d7dd936ccecbaf3a7e538
ManishMaster <manish.rsauto@gmail.com>
2023-12-20 15:36:37 +0530

    update testapi

--- app/apitest/page.jsx
+++ app/apitest/page.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+export const revalidate = 0;
+export const dynamic = 'force-dynamic';
 async function getData() {
     const url = `${process.env.NEXT_PUBLIC_DOMAIN_NAME}api/dashboard`
-    const res = await fetch(url,{ cache: 'force-cache' })
+    const res = await fetch(url,{
+        catch: 'no-cache',
+        next: { revalidate: 1 },
+      })
     // The return value is *not* serialized
     // You can return Date, Map, Set, etc.
    
