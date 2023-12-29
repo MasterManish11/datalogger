ba0792d80e83a5678d239db62399b19d6a2ea026
ManishMaster <manish.rsauto@gmail.com>
2023-12-29 13:28:25 +0530

    simplified logic

--- app/reports/summary/production/page.jsx
+++ app/reports/summary/production/page.jsx
@@ -14,7 +14,7 @@ const ProductionSummaryReport = () => {
   const state = {
     options: [
       { name: "1", id: 1 },
-      { name: "2", id: 2 }, px-2
+      { name: "2", id: 2 },
       //   { name: "3", id: 3 },
       //   { name: "4", id: 4 },
       //   { name: "5", id: 5 },
